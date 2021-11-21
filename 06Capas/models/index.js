d46dac3c00daafbc755df37dfa3c9779ca8c8e9b
santiagodelgado1502 <santiagodelgado1502@hotmail.com>
2021-11-21 15:16:03 -0500

    .

---
+++ 06Capas/models/index.js
@@ -0,0 +1,8 @@
+const Producto =  require('./producto');
+const Categoria =  require('./categoria');
+
+
+module.exports ={
+    Producto,
+    Categoria,
+}
---
+++ 0Introduccion/index.js
@@ -0,0 +1,15 @@
+const { funcion1, variable, saludar } = require('./funcionesmat')
+
+let resultado = funcion1(5, variable, "sumar");
+console.log(saludar("Santiago"))
+console.log(resultado);
+
+/*
+let numero=5
+
+for(let i=0 ; i<5 ; i++)
+{
+
+    console.log(`Actualmente I vale ${i}`);    
+
+}*/
